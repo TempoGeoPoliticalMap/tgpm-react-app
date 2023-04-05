92026ed8d969510a6de0c25d0a26704fe38ceb47
Valentin Grigoryevskiy <v.grigoryevskiy@gmail.com>
2023-04-06 00:22:50 +0100

    roll-back next.config.js
    Signed-off-by: Valentin Grigoryevskiy <v.grigoryevskiy@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
+  output: "standalone"
 };
 
 module.exports = nextConfig;
