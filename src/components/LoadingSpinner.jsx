288af849fc168b679614bde99c09a4dc1f49f3c2
Yuba Neupane <neopaneyb2@gmail.com>
2023-09-11 10:58:13 -0400

    Update src/components/LoadingSpinner.jsx
    Co-authored-by: Valentin Grigoryevsky <29351656+vgrigoryevsky@users.noreply.github.com>

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LoadingSpinner() {
   return (
     <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
-      <div className="border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-64 w-64"></div>
+      <div className="border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-16 w-16" />
     </div>
   );
 }
